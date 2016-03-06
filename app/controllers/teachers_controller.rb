76085eb4c360caa3f047e3fa83d70cb83f3f1069
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-03-06 08:45:30 +0000

    TeachersControllerのAction設定

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,4 +1,24 @@
 class TeachersController < ApplicationController
+　
+　#初期表示
   def index
+  end
+  # データ一覧の画面表示
+  def show
+  end
+  # データ作成の画面表示
+  def new
+  end
+  # データ更新の画面表示
+  def edit
+  end
+  # データを作成
+  def create
+  end
+  # データを更新
+  def update
+  end
+  # データを削除
+  def destroy
   end
 end
