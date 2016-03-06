d10f69098609a9d4c9bef57e1765bd76968953db
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-03-06 04:34:53 +0000

    teacherテーブル作成

--- db/migrate/20160306040940_create_teachers.rb
+++ db/migrate/20160306040940_create_teachers.rb
@@ -1,7 +1,10 @@
 class CreateTeachers < ActiveRecord::Migration
   def change
     create_table :teachers do |t|
-
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :tel
       t.timestamps null: false
     end
   end
