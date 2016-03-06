9607940f5ef54f1050d448106440e96e9a660acc
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-03-06 07:12:01 +0000

    カラムをnot nullに設定

--- db/migrate/20160306040940_create_teachers.rb
+++ db/migrate/20160306040940_create_teachers.rb
@@ -1,11 +1,12 @@
 class CreateTeachers < ActiveRecord::Migration
   def change
     create_table :teachers do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.string :tel
+      t.string :name , null: false
+      t.string :email , null: false
+      t.string :password , null: false
+      t.string :tel , null: false
       t.timestamps null: false
     end
   end
 end
+ra
--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: teachers
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  password   :string           not null
+#  tel        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TeacherTest < ActiveSupport::TestCase
