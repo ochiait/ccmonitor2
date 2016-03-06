9a60f81eda66f2476411044903dd44ad96502dc7
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-03-06 09:52:55 +0000

    不要なAction,Veiwを削除

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,24 +1,10 @@
 class TeachersController < ApplicationController
-　
-　#初期表示
-  def index
-  end
-  # データ一覧の画面表示
-  def show
-  end
   # データ作成の画面表示
   def new
-  end
-  # データ更新の画面表示
-  def edit
+    #Teacherクラス（model）を空で生成し、@teacherというインスタンス変数に格納（箱を作る）
+    @teacher = Teacher.new
   end
   # データを作成
   def create
-  end
-  # データを更新
-  def update
-  end
-  # データを削除
-  def destroy
   end
 end
