2ee15c581786a406729a9981c321ce7f7b393a83
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-03-10 13:02:55 +0000

    lessons_controller修正

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,18 @@
 class LessonsController < ApplicationController
+    # データ作成の画面表示
   def new
+    #lesson modelを空で生成し、@lessonというインスタンス変数に格納（箱を作る）
+    @lesson = Lesson.new
   end
+  # データを作成
+  def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.save
+    render :thanks
+  end
+  
+  def lesson_params
+    params.require(:lesson).permit(:date, :start_time, :end_time, :name, :price, :name, :comment)
+  end  
+  
 end
