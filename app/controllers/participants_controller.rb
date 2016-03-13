c871be8e9fc3fb0684599d59f0d3efe83528b7af
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-03-13 10:13:32 +0000

    participantsコントローラー修正

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,4 +1,17 @@
 class ParticipantsController < ApplicationController
+     # データ作成の画面表示
   def new
+    #participant modelを空で生成し、@participantというインスタンス変数に格納（箱を作る）
+    @participant = Participant.new
   end
+  # データを作成
+  def create
+    @participant = Participant.new(participant_params)
+    @participant.save
+    render :thanks
+  end
+  
+  def participant_params
+    params.require(:participant).permit(:name, :email, :tel)
+  end  
 end
