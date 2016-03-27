7e6f16f874ad74aefcb3a64174738556d7051924
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2016-03-27 11:09:34 +0000

    ログイン機能実装（９）エラー修正3

--- db/migrate/20160322125112_add_password_confirmation_to_teachers.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordConfirmationToTeachers < ActiveRecord::Migration
-  def change
-    add_column :teachers, :password_confirmation, :srting
-  end
-end
--- db/migrate/20160324130245_remove_password_to_teachers.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordToTeachers < ActiveRecord::Migration
-  def change
-    remove_column :teachers, :password, :string
-  end
-end
